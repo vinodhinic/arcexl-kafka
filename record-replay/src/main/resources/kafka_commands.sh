a42d3edefd1effc42725cc1bf7b67c4a609b3e8d
vinodhinic <vinodhinic@gmail.com>
2020-06-07 23:00:35 +0530

    Commands for demo

--- record-replay/src/main/resources/kafka_commands.sh
+++ record-replay/src/main/resources/kafka_commands.sh
@@ -15,3 +15,10 @@ bin/kafka-consumer-groups.sh --bootstrap-server 127.0.0.1:9092 --list
 
 # Describe Consumer groups
 bin/kafka-consumer-groups.sh --bootstrap-server 127.0.0.1:9092 --group stock-price-app --describe
+
+# Commands to demonstrate Compaction
+bin/kafka-topics.sh --zookeeper 127.0.0.1:2181 --create --topic stock-price --partitions 1 --replication-factor 1 --config cleanup.policy=compact --config segment.ms=5000 --config min.cleanable.dirty.ratio=0.001
+
+bin/kafka-console-consumer.sh --bootstrap-server 127.0.0.1:9092 --topic stock-price --from-beginning --property print.key=true --property key.separator=,
+
+bin/kafka-console-producer.sh --broker-list 127.0.0.1:9092 --topic stock-price --property parse.key=true --property key.separator=,
